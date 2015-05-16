2c9e80125d3ffeed059903bb73b39a4e9c8d9b45
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2015-05-16 13:00:04 -0700

    Implemented wildcard "*" and single sid selection syntax. Implement multiple requests in flight to test asynchronous implementation. Add error detection on the result of parsing the object to XML. Rework tqlParse logic and design flow.

--- helloTQL.js
+++ helloTQL.js
@@ -2,13 +2,13 @@ TQL = require('./lib/tql');
 
 
 console.log('Welcome to Hello TQL, JavaScript.');
-console.log('Getting all parking spaces.');
+console.log('Get parking spaces.');
 
 //
 // establish a TQL connection context
 //
 tqlConnection = new TQL.Connection(
-	{url: 'http://cisco.atomiton.com:8080/fid-CIMTQLInterface-JVH2ZQ7YAAAH6AABAE2I4NJR'}
+    {url: 'http://cisco.atomiton.com:8080/fid-CIMTQLInterface-JVH2ZQ7YAAAH6AABAE2I4NJR'}
 );
 
 //
@@ -18,22 +18,39 @@ tqlConnection = new TQL.Connection(
 //
 /*
 parkingSpaces = tqlConnection.request.select({
-	resources: {parkingSpace: '*'}
+    resources: {parkingSpace: '*'}
 });
 // */
-parkingSpaces = tqlConnection.request({
-	select: {
-		parkingSpace: '*'
-	}
+request1 = {select: {parkingspace: '*'}};
+request2 = {select: {parkingspace: 'WorldSensing.5385fc250cf2497dfe5679d1'}};
+request3 = {select: {$or: [
+    {parkingspace: "WorldSensing.5385fc250cf2497dfe5679d1"},
+    {parkingspace: "WorldSensing.5385ff2f0cf2497dfe567c0c"}
+    ]}
+};
+
+[request1, request2].forEach(function(req) {
+    var parkingSpaces = tqlConnection.request(req);
+    parkingSpaces.then(function(response) {
+        console.log("Executed:", req);
+        console.log("parking spaces found:", response.result.length);
+    }).catch(function(error) {
+        console.log('Error class:', error.class, ', error:', error.text);
+    })
 });
 
+/*
+
+parkingSpaces = tqlConnection.request(request2);
+
 parkingSpaces.then(function(response) {
-	console.log('SELECT:', response.status);
-	var result = JSON.stringify(response.result, null, 4);
-	console.log(result);
+    console.log('SELECT:', response.status);
+    var result = JSON.stringify(response.result, null, 4);
+    console.log(result);
 }).catch(function(error) {
-	console.log('Error class:', error.class, ', error:', error.text);
+    console.log('Error class:', error.class, ', error:', error.text);
 })
+// */
 
 //
 // this used the synchronous version of the function so parkingSpaces returns
@@ -41,9 +58,9 @@ parkingSpaces.then(function(response) {
 //
 /*
 if (parkingSpaces.error) {
-	console.log('Error:', parkingSpaces.error.code, parkingSpaces.error.text);
+    console.log('Error:', parkingSpaces.error.code, parkingSpaces.error.text);
 } else {
-	console.log(parkingSpaces.url);
-	console.log(parkingSpaces.selection);
+    console.log(parkingSpaces.url);
+    console.log(parkingSpaces.selection);
 }
 // */
